d6763c6e05196ba6478d48cbc199d1ea1722a8d6
modernProteus <nick.proteus@gmail.com>
2025-07-19 21:32:35 -0500

    Update init_postgres.py

--- backend/db/init_postgres.py
+++ backend/db/init_postgres.py
@@ -3,6 +3,8 @@
 from backend.db.models import db
 from backend.app import app
 
+print(f"📡 Using DB: {app.config['SQLALCHEMY_DATABASE_URI']}")
+
 with app.app_context():
     db.create_all()
     print("✅ Tables created in PostgreSQL.")
